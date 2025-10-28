0b6b6e7b4c3bda0133b1b3ba7a790db97cbdf12e
roaa shamia <enroaashamia@gmail.com>
2025-10-28 21:39:33 +0300

    fixed swiper

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const swiper = new Swiper(".swiper", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        }, breakpoints: {
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 30,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
         },
-    });
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
 });
+
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const sidebar = document.getElementById("sidebar");
